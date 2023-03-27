cf465b7fad7ae110b3f036fc8f5b9d63aaffb443
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-03-27 14:00:44 +0330

    add RoomLocation

--- components/pages/AdvertiseDetails/RoomLocation.jsx
+++ components/pages/AdvertiseDetails/RoomLocation.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function RoomLocation() {
-  return <div>RoomLocation</div>;
+  return (
+    <div>
+      <div className="my-6 text-2xl">موقعیت خانه</div>
+      <div className="h-96">
+        <MapContainer
+          style={{ height: "100%" }}
+          center={[35.7219, 51.3347]}
+          zoom={10}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
+      <div className="mt-4 space-y-3">
+        <div className="text-lg">رامسر</div>
+        <div className="font-light">
+          شهر بسیار آرام، شهرهای کوچک در فاصله چند قدمی، شهرهای بزرگتر در 10
+          دقیقه .
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RoomLocation;
--- pages/[id].js
+++ pages/[id].js
@@ -1,21 +1,31 @@
 import DetailsLayout from "@com-layouts/DetailsLayout";
 import HostInfo from "@com-pages/AdvertiseDetails/HostInfo";
-import RoomLocation from "@com-pages/AdvertiseDetails/RoomLocation";
 import RoomMoreInfo from "@com-pages/AdvertiseDetails/RoomMoreInfo";
 import RoomReviews from "@com-pages/AdvertiseDetails/RoomReviews";
 import React from "react";
 import RoomReservation from "@com-pages/AdvertiseDetails/RoomReservation";
 import RoomInfo from "@com-pages/AdvertiseDetails/RoomInfo";
 
+import dynamic from "next/dynamic";
+const RoomLocation = dynamic(
+  () => import("@com-pages/AdvertiseDetails/RoomLocation"),
+  {
+    ssr: false,
+  }
+);
 function AdvertiseDetails() {
   return (
     <div className="container">
       <RoomInfo />
       <hr className="my-8 border-dashed" />
       <RoomReservation />
+      <hr className="my-8" />
       <RoomReviews />
+      <hr className="my-8" />
       <RoomLocation />
+      <hr className="my-8" />
       <HostInfo />
+      <hr className="my-8" />
       <RoomMoreInfo />
     </div>
   );
